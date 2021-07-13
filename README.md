5bd406825d0722db4d9b5097de560ec5e8c9235d
argoneus <argoneuscze@gmail.com>
2021-07-13 12:59:02 +0200

    first release, cleanup

---
+++ README.md
@@ -0,0 +1,12 @@
+# StriveUtils
+
+This is a tool that (for now) replaces the P2 RISC gauge with P2 Wall damage.
+
+It's still a very barebones prototype, but it should work.
+
+## How to use
+
+- Download [.NET 5 runtime](https://dotnet.microsoft.com/download/dotnet/thank-you/runtime-desktop-5.0.7-windows-x64-installer)
+- Download the [latest release](https://github.com/argoneuscze/StriveUtils/releases/latest)
+- Load up the game into practice mode
+- Run the .exe
--- StriveUtil/Program.cs
+++ StriveUtil/Program.cs
@@ -1,16 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace StriveUtil
 {
     class Program
     {
+
+        private static readonly Tuple<int, int[]> bloodGauge = Tuple.Create(0x4C6E4F8, new int[] { 0x58, 0x4D8, 0x288, 0x178, 0x0, 0x6DC });
+        private static readonly Tuple<int, int[]> wallOnP2 = Tuple.Create(0x4C6E4F8, new int[] { 0x110, 0x8, 0xCFA0 });
+        private static readonly Tuple<int, int[]> riscOnP2 = Tuple.Create(0x4C6E4F8, new int[] { 0x110, 0x8, 0xC424 });
+
         static void Main(string[] args)
         {
-            var test = MemoryLib.MemoryHandler.OpenProcessByName("GGST-Win64-Shipping", true);
-            IntPtr bloodGauge = test.GetAddressWithOffsets(0x4C6E4F8, new int[] { 0x58, 0x4D8, 0x288, 0x178, 0x0, 0x6DC });
-            // var value = test.ReadMemory<int>(bloodGauge);
-            // Console.Out.WriteLine(value);
-            test.WriteMemory<int>(bloodGauge, 5000);
+            var strive = MemoryLib.MemoryHandler.OpenProcessByName("GGST-Win64-Shipping", true);
+            IntPtr wallP2Ptr = strive.GetAddressWithOffsets(wallOnP2.Item1, wallOnP2.Item2);
+            IntPtr riscP2Ptr = strive.GetAddressWithOffsets(riscOnP2.Item1, riscOnP2.Item2);
+
+            int wallP2;
+            int riscP2;
+            int newRisc;
+
+            Console.WriteLine("Process found and tool running.");
+            // Console.WriteLine("P2WALL: {0:X}, P2RISC: {1:X}", wallP2Ptr, riscP2Ptr);
+
+            while (true)
+            {
+                wallP2 = strive.ReadMemory<int>(wallP2Ptr);
+                riscP2 = strive.ReadMemory<int>(riscP2Ptr);
+                newRisc = Math.Min((int)Math.Round(wallP2 * 4.26), 12800);
+                strive.WriteMemory<int>(riscP2Ptr, newRisc);
+            }
         }
     }
 }
